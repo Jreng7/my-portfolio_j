d75a210e3d826529d1ea426b03d0e25b321a5089
Josue R S Jacinto <jr.waydev@gmail.com>
2023-05-07 13:59:12 -0300

    alteracoes

--- src/shared/components/main-header/index.tsx
+++ src/shared/components/main-header/index.tsx
@@ -4,22 +4,8 @@ import profileJ from './assets/perfil-josue.png'
 
 export default function MainHeader() {
     return (
-        <header className="w-full lg:w-[26.5rem] xl:w-[34.25rem] flex flex-col justify-center items-center h-screen bg-gray-100">
-            <div className="flex gap-3  items-center">
-                <Image 
-                src={profileJ} 
-                alt="Foto de perfil do Josué Ribeiro" 
-                className="w-[5.5rem] h-[5.5rem]"
-                />
-                <div>
-                    <Text as="h1" variant="3xl" className='text-gray-800'>
-                        Josué Ribeiro
-                    </Text>
-                    <Text as="div" variant="xs" className="text-gray-500 italic -mt-0.5">
-                        Developer Fullstack @2023/Jrway
-                    </Text>
-                </div>
-            </div>
+        <header className="flex h-screen bg-gray-100">
+            
         </header>
     )
 }
