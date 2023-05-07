ba7830d4d67c1b81d8a8f7c5be1e4c58d18ba903
Josue R S Jacinto <jr.waydev@gmail.com>
2023-05-07 18:34:24 -0300

    main-footer add

--- src/shared/components/main-header/index.tsx
+++ src/shared/components/main-header/index.tsx
@@ -1,8 +1,40 @@
+import Image from 'next/image';
+import { Text } from 'thon-ui';
+import profilePicture from './assets/perfil-josue.png';
 
 export default function MainHeader() {
     return (
-        <header className="w-full lg:w-[26rem] xl:w-[27rem] flex items-center h-screen bg-gray-100">
-            Josué Ribeiro
+        <header className={`
+                w-full h-screen lg:w-[26.5rem] xl:w-[34.25rem]
+                pt-8 pb-6 px-7 lg:pt-0 lg:pb-0
+                flex flex-col justify-center items-center 
+                bg-gray-100
+
+            `}>
+
+            <div className='flex gap-4 items-center'>
+                <Image 
+                    src={profilePicture} 
+                    alt="Foto de perfil Josué Ribeiro Jacinto"
+                    className='lg:mr-2'
+                />
+                <div>
+                    <Text as="h1" variant="3xl" className="text-gray-800">
+                        Josué Ribeiro
+                    </Text>
+                    <Text as="div" variant="xs" className="text-gray-800 italic -mt-0.5">
+                        Fullstack Engineer@JrwayRibeiro
+                    </Text>
+                </div>
+            </div>
+
+            <div className="w-full lg:w-[20rem] mt-6 lg:mt-8">
+                <Text as="p" variant="sm" className="italic text-gray-500">
+                    Criando coisas incríveis desde 2012, com muita atenção aos detalhes,
+                    sentimento de dono e exercendo o feedback.
+                </Text>
+                <Text as="p" variant="sm" className="italic text-gray-500 mt-2">Você pode me chamar de Jo. 😃</Text>
+            </div>
         </header>
-    )   
+        )   
 }   
